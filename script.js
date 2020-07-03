0d4627b76a79d05d59204398b1ecc00195975d93
leonbatista <55902667+leonbatista@users.noreply.github.com>
2020-07-03 14:33:22 -0700

    only tab animation is left

--- script.js
+++ script.js
@@ -6,12 +6,40 @@ tabs.forEach(tab => {
     const target = document.querySelector(tab.dataset.tabTarget)
     tabContents.forEach(tabContent => {
       tabContent.classList.remove('active')
+      document.documentElement.scrollTop = 0;
+      
     })
     tabs.forEach(tab => {
       tab.classList.remove('active')
+
     })
     tab.classList.add('active')
     target.classList.add('active')
+
   })
+})
+
+const button = document.getElementById('home-button');
+const gallery = document.getElementById('gallerytab');
+const home = document.getElementById('hometab')
+const gallerycont = document.getElementById('gallery');
+const homecont = document.getElementById('home-text');
+button.addEventListener("click",function(){ 
+    home.classList.remove('active')
+    gallery.classList.add('active')
+    homecont.classList.remove('active')
+    gallerycont.classList.add('active')
+})
+
+const submit = document.getElementById('sub-button');
+const name = document.getElementById('text-field')
+const emailtext = document.getElementById('text-email')
+const textarea = document.getElementById('text-area')
+submit.addEventListener('click',function(){
+    if(name.value != "" && textarea.value != ""){
+      submit.type = 'reset'
+    }
+    console.log(name.value)
+    console.log(textarea.value)
 })
 
